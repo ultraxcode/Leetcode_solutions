056e5656c5813389fc0d8d4c10caaa973871c2e2
ultraxcode <anuragx19@gmail.com>
2022-03-12 21:35:38 +0530

    191 Number of 1 bits

---
+++ bits1n.java
@@ -0,0 +1,13 @@
+public class bits1n {
+    public static void main(String[] args) {
+        public class Solution {
+            // you need to treat n as an unsigned value
+            public int hammingWeight(int n) {
+            return Integer.bitCount(n);
+            }
+            }
+            
+        
+    }
+    
+}
